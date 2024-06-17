6b54ec541b8b4e85f84a0884270facd60a1880bc
JJ Espinoza <4843736+espin086@users.noreply.github.com>
2024-06-17 14:42:00 -0700

    updated the gitignore to remove the secrets before pushing

---
+++ config.py
@@ -0,0 +1,10 @@
+GPT_MODEL = "gpt-4o"
+
+
+class UserConfig:
+    """Configurations for the user"""
+
+    def __init__(self):
+        self.name = "JJ"
+        self.job_title = "Scientist"
+        self.company = "Acme Corporation"
